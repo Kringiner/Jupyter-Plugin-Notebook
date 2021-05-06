28d323ed4ebc8bde81f9fb91c51b95897954fc1f
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-05-06 20:35:03 +0500

    Created simple JS-extension

---
+++ pretty-python/README.md
@@ -0,0 +1,14 @@
+Pretty  python
+=========
+To use the extension, do three simple things:
+1) Run this command to install Jupyter nbextensions
+
+    `pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install`
+
+2) Run this command to search directory with nbextensions:
+
+    `pip show jupyter_contrib_nbextensions`
+   
+3) Move directory `pretty-python` to directory with nbextensions
+
+Done! It remains only to connect it already in Jupyter.
---
+++ pretty-python/main.js
@@ -0,0 +1,44 @@
+define([
+    'base/js/namespace',
+    'jquery',
+    'base/js/events',
+], function(Jupyter, $, events) {
+
+    /*
+        Change raw python code on pretty math pseudo-code
+        @param cell - cell with python code
+     */
+    function make_python_pretty(cell) {
+        const html = get_pretty_html_from_code(cell.get_text());
+        const code_wrapper = cell.code_mirror.display.lineDiv;
+        $(code_wrapper).html(html.join(' '));
+    }
+
+     /*
+        Returns wrapped html pretty math code from raw python code
+        @param code - python source code
+     */
+    function get_pretty_html_from_code(code) {
+        return [
+            '<pre class=" CodeMirror-line " role="presentation">',
+                '<span class="cm-variable">c</span>',
+                '<span class="cm-operator">=</span>',
+                '<span class="cm-variable">10</span>',
+                '<span class="cm-operator">⋅</span>',
+                '<span class="cm-variable">12</span>',
+            '</pre>'
+        ];
+    }
+
+    // Run on start
+    function load_ipython_extension() {
+        events.on("execute.CodeCell", function (event, data) {
+            console.log(data.cell);
+            make_python_pretty(data.cell);
+        });
+    }
+
+    return {
+        load_ipython_extension: load_ipython_extension
+    };
+});
