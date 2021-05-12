0d6810677f45ae43a37a160be7ef127eae8a549f
AppleePie <64743297+AppleePie@users.noreply.github.com>
2021-05-13 00:59:41 +0500

    Changed js-extension

--- pretty-python-server/handler.py
+++ pretty-python-server/handler.py
@@ -5,7 +5,8 @@ from notebook.base.handlers import IPythonHandler
 
 
 class PrettyPythonHandler(IPythonHandler, ABC):
-    def get(self):
-        obj = {'key': 'value'}
+    def post(self):
+        obj = self.get_json_body()
         self.set_header("Content-Type", "application/json")
+        self.set_status(200)
         self.write(json.dumps(obj))
--- pretty-python/main.js
+++ pretty-python/main.js
@@ -3,40 +3,48 @@ define([
     'jquery',
     'base/js/events',
 ], function (Jupyter, $, events) {
+    const apiRoute = "/pretty-python-server/pretty-code"
 
-    /*
-        Change raw python code on pretty math pseudo-code
-        @param cell - cell with python code
-     */
-    function make_python_pretty(cell) {
-        const html = get_pretty_html_from_code(cell.get_text());
-        const code_wrapper = cell.code_mirror.display.lineDiv;
-        $(code_wrapper).html(
-            '<pre class=" CodeMirror-line " role="presentation">' +
-                html.join(' ') +
-            '</pre>'
-        );
+    function _get_cookie(name) {
+        const r = document.cookie.match("\\b" + name + "=([^;]*)\\b");
+        return r ? r[1] : undefined;
     }
 
-    /*
-       Returns wrapped html pretty math code from raw python code
-       @param code - python source code
-    */
-    function get_pretty_html_from_code(code) {
+    async function make_python_pretty(cell) {
+        const html = await get_pretty_html_from_code(cell.get_text());
+        const code_wrapper = cell.code_mirror.display.lineDiv;
+        if (cell.get_text() === "c = a * b") {
+            $(code_wrapper).html(
+                '<pre class=" CodeMirror-line " role="presentation">' + html.join(' ') + '</pre>'
+            );
+        }
+    }
+
+    async function get_pretty_html_from_code(code) {
+        const config = {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {'Content-Type': 'application/json', "X-XSRFToken": _get_cookie('_xsrf')},
+            body: JSON.stringify({code})
+        };
+        const json = await fetch(apiRoute, config)
+            .then(p => p.json())
+            .catch(e => console.error(e));
         return [
             '<span class="cm-variable">c</span>',
             '<span class="cm-operator">=</span>',
-            '<span class="cm-variable">10</span>',
+            '<span class="cm-variable">a</span>',
             '<span class="cm-operator">⋅</span>',
-            '<span class="cm-variable">12</span>',
+            '<span class="cm-variable">b</span>',
         ];
     }
 
     // Run on start
-    function load_ipython_extension() {
-        events.on("execute.CodeCell", function (event, data) {
-            console.log(data.cell);
-            make_python_pretty(data.cell);
+    async function load_ipython_extension() {
+        events.on("execute.CodeCell", async (event, data) => {
+            await make_python_pretty(data.cell);
         });
     }
 
